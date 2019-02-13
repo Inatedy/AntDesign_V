2e1bcd144c6369a2f85823212aa969f73e1293cc
Martin Melo <nitram-210397@hotmail.com>
2019-02-13 11:04:10 -0600

    Header ready with router

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Home = () => (
+    <h2>Hola bienvenido</h2>
+)
+
+export default Home
---
+++ src/components/patients/Patients.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Patients = () => (
+    <h2>Pacientes en espera</h2>
+)
+
+export default Patients
